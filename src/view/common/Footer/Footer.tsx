17b43cc9fd92418f8b3e0a6b08a9dc8ecf860192
jgkin <himanthagamachchige@gmail.com>
2025-06-07 10:18:54 +0530

    div cards implemented and loaded with josn objects cart implement

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -1,19 +1,25 @@
 // import './Footer.css'
 import {Link} from "react-router-dom";
-import logo from "../../../assets/nav.png";
+import logo from "../../../assets/keels.jpg";
 export function Footer() {
     return (
-        <div className="flex items-center justify-between p-4 bg-gradient-to-r from-blue-500 to-green-400 text-white shadow-md">
-            <img className="w-20 h-20 cursor-pointer" src={logo} alt="logo" />
-            <p className="text-xl font-bold text-white hover:text-black">My Website</p>
+        <div
+            className="flex items-center justify-between p-4 bg-gradient-to-r from-blue-500 to-green-400 text-white shadow-md">
+            <img className="w-20 h-20 cursor-pointer" src={logo} alt="logo"/>
+            <p className="text-xl font-bold text-white hover:text-black">Keels super</p>
             <ul className="flex gap-8">
                 <li><Link className="text-white hover:text-black font-bold" to="/">Home</Link></li>
                 <li><Link className="text-white hover:text-black font-bold" to="/about">About</Link></li>
                 <li><Link className="text-white hover:text-black font-bold" to="/contact">Contact</Link></li>
             </ul>
-            <button className="ml-4 bg-white text-gray-800 px-4 py-2 rounded hover:bg-green-500 hover:text-black">
+            <button
+                className="bg-gradient-to-r from-blue-500 to-green-400 ml-4 bg-white text-gray-800 px-4 py-2 rounded hover:bg-green-500 hover:text-black">
                 <Link to="/login">Sign In</Link>
             </button>
+            <div className="text-center text-gray-600 mt-4 flex flex-col items-center">
+                <p className="text-xs">© 2025 Keels Super. All rights reserved.</p>
+                <p className="text-xs">Privacy Policy | Terms of Service</p>
+            </div>
         </div>
     );
 }
--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -1,21 +1,24 @@
-import img1 from '../../../assets/products/Beans.jpg';
-import img2 from '../../../assets/products/spinach.jpg';
-import img3 from '../../../assets/products/tomato.jpg';
 import {useEffect, useState} from "react";
 import {Product} from "../../common/product/Product.tsx";
+type ProductData = {
+    id :number,
+    name: string,
+    price: string,
+    currency: string,
+    image: string
+}
 export function Home() {
-    const [products, setProducts] = useState([]);
-
-
+    const [products, setProducts]
+        = useState<ProductData[]>([]);
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('./product-data.json')
                 const jsonData = await response.json();
-                console.log(jsonData);
+                // console.log(jsonData);
                 setProducts(jsonData);
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error('Error fetching data:', error)
             }
         }
         fetchData();
@@ -27,7 +30,7 @@ export function Home() {
                             justify-center items-center mx-auto">
                 {
                     products.map((product) => (
-                     <Product data={product}/>
+                        <Product key={product.id} data={product}/>
                     ))
                 }
             </div>
